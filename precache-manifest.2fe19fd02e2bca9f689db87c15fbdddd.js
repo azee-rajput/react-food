4179a6fc7c3ba62027f2a415a3080138aaa7cb04
azee-rajput <azee.rajput.7@gmail.com>
2019-10-26 16:08:29 +0500

    Updates

---
+++ precache-manifest.2fe19fd02e2bca9f689db87c15fbdddd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9877c0e0e5643f6913ca9fb5611f56d",
+    "url": "/github.io/react-food/index.html"
+  },
+  {
+    "revision": "9e95302a6cb1a2ffaa6f",
+    "url": "/github.io/react-food/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "7f9455bb3a8a2a7428b9",
+    "url": "/github.io/react-food/static/css/main.1995a5c4.chunk.css"
+  },
+  {
+    "revision": "9e95302a6cb1a2ffaa6f",
+    "url": "/github.io/react-food/static/js/2.5624e2ee.chunk.js"
+  },
+  {
+    "revision": "7f9455bb3a8a2a7428b9",
+    "url": "/github.io/react-food/static/js/main.93d08d8f.chunk.js"
+  },
+  {
+    "revision": "74b17d3c5ca63670ac60",
+    "url": "/github.io/react-food/static/js/runtime-main.1c1c674f.js"
+  }
+]);
